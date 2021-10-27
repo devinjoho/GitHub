cedc4dad8af6fc48c94fa5aa5c9329474610f1cf
Devin Johnson Hogan <ucfadj1@ucl.ac.uk>
2021-10-27 13:54:03 +0100

    1st commit

---
+++ Practical_wk4.R
@@ -0,0 +1,20 @@
+library(usethis)
+edit_git_config()
+modify #filepath ('C users Devin .gitconfig')
+# new git tab created 
+install.packages('git')
+library(git)
+
+create_github_token()
+install.packages('gitcreds')
+library(gitcreds)
+gitcreds_set()
+
+token = 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
+
+
+
+
+
+
+
